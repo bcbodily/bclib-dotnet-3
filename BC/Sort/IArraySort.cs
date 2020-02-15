6f8986b8e2149dea4a7775ff0f7ef1bb7582f6a3
Brad Bodily <bradbodily@hotmail.com>
2020-02-15 10:50:34 -0500

    WIP commit - IArraySort docs

--- BC/Sort/IArraySort.cs
+++ BC/Sort/IArraySort.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace BC.Sort
 {
+    /// <summary>
+    /// Interface to an object that can sort arrays
+    /// </summary>
+    /// <typeparam name="T">the type of element the sorting object works with</typeparam>
     public interface IArraySort<T> where T : IComparable<T>
     {
+        /// <summary>
+        /// Sorts a specified array
+        /// </summary>
+        /// <param name="array">the array to sort</param>
         public void Sort(T[] array);
     }
 }
