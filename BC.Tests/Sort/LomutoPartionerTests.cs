f097651422bc691fa912510479b1fc21131bbeba
Brad Bodily <bradbodily@hotmail.com>
2020-02-15 10:19:06 -0500

    WIP commit - Lomuto tests and updated Test project
    Added test for LomutoPartitioner
    Updated BC.Tests project with Moq dependency

---
+++ BC.Tests/Sort/LomutoPartionerTests.cs
@@ -0,0 +1,61 @@
+using System;
+using Xunit;
+
+namespace BC.Sort
+{
+    public class LomutoPartionerTests
+    {
+        /// <summary>
+        /// Verifies <see cref="LomutoPartioner.Partition(ref T[], int, int)"/> properly returns a partition index for which all elements that come before the partition index are less than or equal to the element stored at the partition index
+        /// </summary>
+        /// <param name="size">the size of the array to create for testing</param>
+        [Theory]
+        [InlineData(10)]
+        public void Partition_all_elements_before_partition_are_equal_to_or_less_than(int size)
+        {
+            var array = GetTestArray(size);
+
+            var partition = new LomutoPartioner<int>(new SortHelper<int>()).Partition(ref array, 0, array.Length - 1);
+            var partitionValue = array[partition];
+            for (int i = 0; i < partition; i++)
+            {
+                Assert.True(array[i].CompareTo(partitionValue) <= 0);
+            }
+        }
+
+        /// <summary>
+        /// Verifies <see cref="LomutoPartioner.Partition(ref T[], int, int)"/> properly returns a partition index for which all elements that come after the partition index are greater than or equal to the element stored at the partition index
+        /// </summary>
+        /// <param name="size">the size of the array to create for testing</param>
+        [Theory]
+        [InlineData(10)]
+        public void Partition_all_elements_after_partition_are_greater_than_or_equal_to(int size)
+        {
+            var array = GetTestArray(size);
+
+            var partition = new LomutoPartioner<int>(new SortHelper<int>()).Partition(ref array, 0, array.Length - 1);
+            var partitionValue = array[partition];
+            for (int i = partition + 1; i < array.Length; i++)
+            {
+                Assert.False(array[i].CompareTo(partitionValue) >= 0);
+            }
+        }
+
+        /// <summary>
+        /// Gets an array if ints, with no two elements equal
+        /// </summary>
+        /// <param name="size">the size of the array to create</param>
+        /// <returns>an int array of the specified size, with no two elements equal</returns>
+        private int[] GetTestArray(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
+        }
+    }
+}
--- BC/Sort/IPartionerTwoWay.cs
+++ BC/Sort/IPartionerTwoWay.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace BC.Sort
 {
+    /// <summary>
+    /// An interface to a partioner that partions an array into two parts, separated by a single index
+    /// </summary>
+    /// <typeparam name="T">the type of elements in the array being partioned</typeparam>
     public interface IPartionerTwoWay<T> where T : IComparable<T>
     {
+        /// <summary>
+        /// Partions the array, and returns the index of the element that separates the two parts
+        /// </summary>
+        /// <param name="array">the array to be partioned</param>
+        /// <param name="left">the index of the left-most element of the portion of the array to be partioned</param>
+        /// <param name="right">the index of the right-most element of the portion of the array to be partioned</param>
+        /// <returns>the index of the element that separates the two parts</returns>
         int Partition(ref T[] array, int left, int right);
     }
 }
